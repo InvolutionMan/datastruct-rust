d00cd259d4a0a10fcab8e4656a0d590223cd8553
InvolutionMan <2096254817@qq.com>
2026-07-05 21:04:19 +0800

    Update list.rs

--- src/bin/list.rs
+++ src/bin/list.rs
@@ -1,3 +1,29 @@
+#[derive(Debug)]
+struct Node<T>
+{
+  value:T,
+  next:Option<Box<Node<T>>>
+}
+impl <T> Node<T> {
+    fn new(value:T)->Self{
+      Node{value,next:None} //默认为空
+    }
+}
+struct LinkList<T>
+{
+  head:Option<Box<Node<T>>>,
+  size:usize
+}
+impl <T> LinkList<T> {
+  fn new()->Self {
+    LinkList { head: None, size: 0 }
+  }
+    
+}
+//头插法
+
+//尾插法 
+
 fn main() {
-  println!("hello")
+    
 }
