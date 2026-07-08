3b469de81ee1207c71860e1f52d9a2530f313f2d
InvolutionMan <2096254817@qq.com>
2026-07-08 21:59:33 +0800

    114514

--- src/bin/list.rs
+++ src/bin/list.rs
@@ -1,4 +1,4 @@
-use std::thread::current;
+use std::{hash::RandomState, ops::Index, sync::mpsc::TrySendError};
 
 #[derive(Debug)]
 struct Node<T> {
@@ -42,24 +42,26 @@ impl<T> LinkList<T> {
                     //ref  借可变引用
                     current = next;
                 }
-                current.next=Some(new_node);
+                current.next = Some(new_node);
             }
         }
         self.size += 1;
     }
     // 打印链表
-    fn print_list(&self) where T:std::fmt::Display
+    fn print_list(&self)
+    where
+        T: std::fmt::Display,
     {
-      let mut temp=&self.head;
-      while let Some(node)=temp{
-        print!("{}->",node.value);
-        temp=&node.next;
-      }
-      println!("None");
-      println!("链表的长度为{}",&self.size);
+        let mut temp = &self.head;
+        while let Some(node) = temp {
+            print!("{}->", node.value);
+            temp = &node.next;
+        }
+        println!("None");
+        println!("链表的长度为{}", &self.size);
     }
     // 反转链表
-    fn reverselist (&mut self) 
+    fn reverselist (&mut self) where T:std::fmt::Display
     {
         let mut prev=None;
         let mut current=self.head.take();
@@ -68,14 +70,72 @@ impl<T> LinkList<T> {
           let next=node.next.take();
           node.next=prev;
           prev=Some(node);
-          current=next
+          current=next;
         }
         self.head=prev;
+        
     }
     // 按值查找
+    fn find_by_value(&self,value: T) ->bool
+    where T:PartialEq,
+    {
+      let mut current=&self.head;
+      while let Some(mut node) =current.as_ref() 
+      {
+          if node.value==value
+          {
+            return true;
+          }
+          current=&node.next;
+      }
+      return false;
+    }
     // 按索引查找
-    // 插入节点
-    
+    fn find_by_index(&self,index:usize) ->bool
+    {
+      let mut current=&self.head;
+      let mut i=0;
+      while let Some(node) =current   
+      {
+          if i==index
+          {
+            return true;
+          }
+          current=&node.next;
+          i+=1;
+      }
+      return false;
+    }
+    //通过值删除节点
+    fn delete_node_by_value(&mut self,value: &T) ->bool where T:PartialEq
+    {  
+      let mut current=&mut self.head;
+      if let Some(node)=self.head.as_ref() //as_ref 不可变引用
+      {
+        if node.value==*value
+        {
+          self.head = self.head.take().unwrap().next;
+          self.size-=1;
+          return true;
+        }
+      }
+      while let Some(node)=current.as_mut() // as_mut可变引用
+      {
+        if let Some(next_node)=node.next.as_ref()
+        {
+          if next_node.value==*value
+          {
+            let mut remove=node.next.take().unwrap();
+            node.next=remove.next;
+            self.size-=1;
+            return true;
+          }
+        }
+        current=&mut node.next;
+      }
+      return false;
+    }
+    //通过索引删除节点
 }
 
 fn main() {
@@ -83,4 +143,7 @@ fn main() {
   list.push_front(1);
   list.push_tail(2);
   list.print_list();
+  println!("是否找到节点，{}",list.find_by_value(1));
+  println!("是否找到节点，{}",list.find_by_index(1));
+  
 }
